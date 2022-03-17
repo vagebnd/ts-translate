1d906409cf71e75e75d2c6ecb33597eae7f9901e
Gianluca Riggio <luca@maximumawesome.nl>
2022-03-17 14:19:50 +0100

    added translator

---
+++ src/classes/translator.ts
@@ -0,0 +1,41 @@
+import Config from './config'
+import Finder from '/@/classes/finder'
+import inquirer from 'inquirer'
+
+class Translator {
+  private config: Config
+  private language: string
+
+  public constructor(config: Config, language: string) {
+    this.config = config
+    this.language = language
+  }
+
+  public listUntranslatedKeys() {
+    const finder = new Finder(this.config)
+    const currentTranslations = this.config.adapter.read(this.language)
+
+    return finder
+      .getTranslatableStrings()
+      .filter((key) => {
+        return !Object.keys(currentTranslations).includes(key as string)
+      })
+      .sort()
+  }
+
+  public async translate() {
+    const prompts = this.listUntranslatedKeys().map((key) => {
+      return {
+        type: 'input',
+        name: key,
+        message: `Please translate: "${key}"`,
+      }
+    })
+
+    const answers = await inquirer.prompt(prompts)
+    this.config.adapter.write(this.language, answers)
+    return answers
+  }
+}
+
+export default Translator
